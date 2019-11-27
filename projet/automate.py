098c21efd9adce8b37f0dab045785ca5d54c44e0
NilsM75 <compte.mxs@hotmail.fr>
2019-11-27 18:00:22 +0100

    ajout de mon projet

---
+++ projet/automate.py
@@ -0,0 +1,325 @@
+# -*- coding: utf-8 -*-
+from transition import *
+from state import *
+import os
+import copy
+from sp import *
+from parser import *
+from itertools import product
+from automateBase import AutomateBase
+
+
+
+class Automate(AutomateBase):
+
+    def succElem(self, state, lettre):
+        """State x str -> list[State]
+        rend la liste des états accessibles à partir d'un état
+        state par l'étiquette lettre
+        """
+        successeurs = []
+        # t: Transitions
+        for t in self.getListTransitionsFrom(state):
+            if t.etiquette == lettre and t.stateDest not in successeurs:
+                successeurs.append(t.stateDest)
+        return successeurs
+
+
+    def succ(self, listStates, lettre):
+        """list[State] x str -> list[State]
+        rend la liste des états accessibles à partir de la liste d'états
+        listStates par l'étiquette lettre
+        """
+        """ ANCIEN CODE QUI POSE PROBLEME : on peut ajouter plusieurs etats dans la meme liste !
+        retour = [] #liste des etats accessibles a partir de la liste d'états
+        for i in self.listStates:
+            for j in self.succElem(i, lettre): #on parcourt toute la liste des etats accessibles part la lettre
+                if j not in retour: #si notre état n'est pas dans la liste
+                    retour.append(j) #on l'ajoute
+        return retour
+        """
+        succ = []
+        ret = []
+        tmp = set()
+        for i in listStates : #On parcourt l'ensemble des etats de (listState)
+            succ = succ + (self.succElem(i,lettre)) #On ajoute a (succ) les etats successeurs des etats de (list)
+            tmp = set(succ) #tmp etant un ensemble, il est donc impossible de lui ajouter plusieurs fois le meme état
+        for j in tmp:
+            ret.append(j)
+        return ret
+
+
+
+    """ Définition d'une fonction déterminant si un mot est accepté par un automate.
+    Exemple :
+            a=Automate.creationAutomate("monAutomate.txt")
+            if Automate.accepte(a,"abc"):
+                print "L'automate accepte le mot abc"
+            else:
+                print "L'automate n'accepte pas le mot abc"
+    """
+    @staticmethod
+    def accepte(auto,mot) :
+        """ Automate x str -> bool
+        rend True si auto accepte mot, False sinon
+        """
+        liste = auto.getListInitialStates() #liste == liste des etats initaux
+        for lettre in mot: #parcourt chaque lettre du mot
+            liste = auto.succ(liste,lettre)
+        #a la fin de la boucle, liste vaudra la liste des derniers états dans lequel la derniere lettre du mot sera atterie
+        #liste sera egale a la liste d'etats a partir duquel on applique le mot
+        for etat in liste :
+            if etat in auto.getListFinalStates(): #on verifie si l'etat est dans la liste des etats finaux
+                return True
+        return False
+
+
+    @staticmethod
+    def estComplet(auto,alphabet) :
+        """ Automate x str -> bool
+         rend True si auto est complet pour alphabet, False sinon
+        """
+        listeEtats = auto.listStates #On recupere la liste de tous les etats de l'automate
+        for lettre in alphabet:  #on parcourt chaque lettre de l'alphabet
+            for etat in listeEtats:  #On parcourt chaque etat de la liste
+                if (auto.succElem(etat,lettre) == []): #si il existe un seul état n'ayant pas de successeurs pour cette lettre (etiquette)
+                    return False                 #l'automate n'est pas complet
+        return True                              #SINON Il le serait
+
+
+
+    @staticmethod
+    def estDeterministe(auto) :
+        """ Automate  -> bool
+        rend True si auto est déterministe, False sinon
+        """
+
+        cpt = 0
+        for etat in auto.listStates : #On parcourt l'ensemble des etats
+            for transi1 in auto.getListTransitionsFrom(etat) : #On parcourt l'ensemble des transitions de l'etat
+                for transi2 in auto.getListTransitionsFrom(etat) : #On parcourt l'ensemble des transitions l'etat
+                    if transi1.etiquette == transi2.etiquette : #Si l'etiquette d'une transition est dans la liste des transitions
+                        cpt = cpt + 1 #On ajoute +1 si l'etiquette d'une transition est dans la liste des transitions
+                        if cpt >= 2 : #Si cpt >= 2, ça veut dire qu'il existe plusieurs transitions avec la meme etiquette partant d'un etat
+                            return False
+                cpt = 0
+        return True
+
+
+
+    @staticmethod
+    def completeAutomate(auto,alphabet) :
+        """ Automate x str -> Automate
+        rend l'automate complété d'auto, par rapport à alphabet
+        on suppose que les ID sont dans l'ordre 0...1..
+        """
+        #Si l'automate est deja complet, inutile de le completer
+        if (auto.estComplet(auto, alphabet)):
+            return auto
+
+        #On crée un nouvel etat "tampon" qu'on ajoutera a la fin de la liste des etats si il n'existe pas
+        nvState = State(len(auto.listStates), False, False)
+        auto.addState(nvState) #On ajoute notre puit à l'automate
+        for lettre in alphabet:
+            for etat in auto.listStates:
+                if (auto.succElem(etat, lettre) == []): #Si il n'existe pas d'etat suivant pour un état donné,
+                    auto.addTransition(Transition(etat, lettre, nvState)) #On le crée
+        return auto
+
+
+
+    @staticmethod
+    def determinisation(auto) :
+        """ Automate  -> Automate
+        rend l'automate déterminisé d'auto
+        """
+        #Si l'automate est déterministe, on retourne auto
+        if (Automate.estDeterministe(auto)) :
+            return auto
+
+        #Liste des états finaux de l'automate de départ
+        liste_etats_finaux = auto.getListFinalStates()
+        #Liste des états d'entree de l'automate de départ
+        liste_etats_initiaux = auto.getListInitialStates()
+        #Liste des etats du nouvel automate sous la forme list[set[State]]
+        liste_new_etat = [set(liste_etats_initiaux)]
+        #La liste des transitions du nouvel automate sous la forme list[ list[set[State],str,set[State]] ]
+        liste_new_transition = []
+        #La liste des états à traiter sous la forme list[list[State]]
+        etats_en_cours = [liste_etats_initiaux]
+        #L'alphabet de l'automate list[str]
+        alphabet = auto.getAlphabetFromTransitions()
+        count_nb_etats = 0
+
+
+        while(len(etats_en_cours) != 0) :
+
+            index_courant = len(etats_en_cours)-1
+            #Pour chaque ensemble d'états dans la liste à traiter
+            while index_courant >= 0 :
+                ens_state = etats_en_cours[index_courant]
+                #Liste des transitions de tous les sous_etat
+                liste_transitions = []
+                #Pour chaque sous_état dans l'ensemble d'états
+                for sous_state in ens_state :
+                    #On prend les transitions du sous état et on les ajoute à la liste
+                    liste_transitions += auto.getListTransitionsFrom(sous_state)
+
+                #Pour chaque lettre de l'alphabet, on va regarder si il y en a une avec une étiquette
+                for etiquette in alphabet :
+                    #La variable qui va contenir l'ensemble des etats suivants
+                    ens_etats_suivants = set()
+                    #Pour chaque transition à partir du sous-etat, on check celle(s) qui est associée à une lettre de l'alphabet
+                    for transition in liste_transitions :
+                        if etiquette == transition.etiquette :
+                            ens_etats_suivants.add(transition.stateDest)
+
+                    #On n'ajoute pas un état vide
+                    if ens_etats_suivants != set() :
+                        #Si l'état n'est pas déjà présent dans la liste des états à traiter, on l'ajoute
+                        #Si l'état n'est pas déjà présent dans la liste des états finaux
+                        if list(ens_etats_suivants) not in etats_en_cours and ens_etats_suivants not in liste_new_etat :
+                            etats_en_cours.append(list(ens_etats_suivants))
+                            liste_new_etat.append(ens_etats_suivants)
+
+                        #On ajoute la transition dans la liste des transisions finales
+                        liste_new_transition.append([set(ens_state),etiquette,ens_etats_suivants])
+
+                #On retire de la liste l'état que l'on vient de traiter
+                etats_en_cours.remove(ens_state)
+                #On décrémente l'index courant
+                index_courant -= 1
+
+
+        #print(liste_new_etat)
+        #print(liste_new_transition)
+        #On a donc liste_new_etat = [ {etat0, etat1, ...} , {etat0, ...} ... ]
+        #Et on a liste_new_transition = [ [{etat0, etat1, ...}, char, {etat0, ...}], ... ]
+
+        #Liste des transitions qui vont nous permettre de construire le nouvel automate
+        liste_transitions_final_simplifie = []
+
+        #Pour chaque transition que l'on a crée précédemment, il nous faut créer les états qui corresondent
+        for transition in liste_new_transition :
+            #Les booléens qui vont renseigner la nature d l'état
+            if_etat_init1 = False
+            if_etat_final1 = False
+            if_etat_init2 = False
+            if_etat_final2 = False
+            for etat in transition[0] :
+                if etat in liste_etats_initiaux :
+                    if_etat_init1 = True
+
+                if etat in liste_etats_finaux :
+                    if_etat_final1 = True
+
+            for etat in transition[2] :
+                if etat in liste_etats_initiaux :
+                    if_etat_init2 = True
+
+                if etat in liste_etats_finaux :
+                    if_etat_final2 = True
+
+            etat1 = State(liste_new_etat.index(transition[0]),if_etat_init1,if_etat_final1,transition[0])
+            etat2 = State(liste_new_etat.index(transition[2]),if_etat_init2,if_etat_final2,transition[2])
+            etiquette = transition[1]
+            liste_transitions_final_simplifie.append(Transition(etat1,etiquette,etat2))
+
+
+        return Automate(liste_transitions_final_simplifie)
+
+
+    @staticmethod
+    def complementaire(auto,alphabet):
+        """ Automate -> Automate
+        rend  l'automate acceptant pour langage le complémentaire du langage de a
+        """
+
+
+    @staticmethod
+    def intersection (auto0, auto1):
+        """ Automate x Automate -> Automate
+        rend l'automate acceptant pour langage l'intersection des langages des deux automates
+        """
+        return
+
+    @staticmethod
+    def union (auto0, auto1):
+        """ Automate x Automate -> Automate
+        rend l'automate acceptant pour langage l'union des langages des deux automates
+        """
+        return
+
+
+
+
+
+    @staticmethod
+    def concatenation (auto1, auto2):
+        """ Automate x Automate -> Automate
+        rend l'automate acceptant pour langage la concaténation des langages des deux automates
+        """
+        return
+
+
+    @staticmethod
+    def etoile (auto):
+        """ Automate  -> Automate
+        rend l'automate acceptant pour langage l'étoile du langage de a
+        """
+        return
+
+## creationAutomate "auto"
+#s0 = State(0, True, False)
+#s1 = State(1, False, False)
+#s2 = State(2, False, True)
+#t1 = Transition(s0,"a",s0)
+#t2 = Transition(s0,"b",s1)
+#t3 = Transition(s1,"a",s2)
+#t4 = Transition(s1,"b",s2)
+#t5 = Transition(s2,"a",s0)
+#t6 = Transition(s2,"b",s1)
+#liste = [t1,t2,t3,t4,t5,t6]
+#auto = Automate(liste)
+
+#print(auto)
+#auto.show("toto") ##Creation pdf avec ce nom "toto"
+
+## creationAutomate "auto1" 2eme manière
+#auto1 = Automate([t1,t2,t3,t4,t5,t6], [s0,s1,s2])
+#print(auto1)
+#auto1.show("auto1")
+
+##Creation automate "auto2" 3eme manière
+#auto2 = Automate.creationAutomate("auto.txt")
+#print(auto2)
+#auto2.show("titi")
+
+##################### PREMIERES MANIPS #####################
+#t = Transition(s0, "a", s1)
+#auto.removeTransition(t)
+#auto.removeTransition(t1)
+#print(auto)
+#auto.show("totomoinst1")
+#auto.addTransition(t1)
+#print(auto)
+#auto.show("totooriginal")
+
+#auto.removeState(s1)
+#print(auto)
+#auto.show("totoSansS1")
+#auto.addState(s1)
+#print(auto)
+#auto.show("totoBis")
+#s2 = State (0, True, False)
+#auto.addState(s2)
+#print(auto)
+#auto.show("totoRemake")
+
+#l = auto1.getListTransitionsFrom(s1)
+#print(l)
+#auto1.show("auto1bis")
+
+#auto2 = Automate.creationAutomate("auto.txt")
+#print(auto2)
+#auto.show(auto2)
---
+++ projet/montest.py
@@ -0,0 +1,330 @@
+from state import *
+from parser import *
+from sp import *
+from transition import *
+from automateBase import *
+from automate import *
+
+#-----------------------Automate A-----------------------
+#-----------------------Automate A-----------------------
+#-----------------------Automate A-----------------------
+print("----------Automate A----------")
+print("----------Automate A----------")
+print("----------Automate A----------")
+#Complet et Déterministe
+#Etats
+aState0 = State(0,True,True)
+aState1 = State(1,False,False)
+aListe_state = [aState0,aState1]
+
+#Transitions
+aTransition0 = Transition(aState0,"a",aState0)
+aTransition1 = Transition(aState0,"b",aState1)
+aTransition2 = Transition(aState1,"a",aState1)
+aTransition3 = Transition(aState1,"b",aState0)
+aListe_trans = [aTransition0,aTransition1,aTransition2,aTransition3]
+
+aAutomate = Automate(aListe_trans)
+aAutomateComplete = Automate(aListe_trans)
+aAutomateDeter = Automate(aListe_trans)
+
+print(aAutomate)
+aAutomate.show("automateA")
+#succ
+print("\n")
+print("----------succ----------")
+print("Quelle est la liste des états à partir de la lettre 'a' ?")
+print(aAutomate.succ(aListe_state,"a"))
+print("Quelle est la liste des états à partir de la lettre 'b' ?")
+print(aAutomate.succ(aListe_state,"b"))
+
+#accepte
+print("\n")
+print("----------accepte----------")
+print("le mot '' (vide) est-il accepté ?")
+print(Automate.accepte(aAutomate,""))
+print("le mot 'a' est-il accepté ?")
+print(Automate.accepte(aAutomate,"a"))
+print("le mot 'b' est-il accepté ?")
+print(Automate.accepte(aAutomate,"b"))
+print("le mot 'aabaabaa' est-il accepté ?")
+print(Automate.accepte(aAutomate,"aabaabaa"))
+print("le mot 'babab' est-il accepté ?")
+print(Automate.accepte(aAutomate,"babab"))
+
+#estComplet
+print("\n")
+print("----------estComplet----------")
+print("l'automate Automate A est-il complet ?")
+print(Automate.estComplet(aAutomate,"ab"))
+
+#estDeterministe
+print("\n")
+print("----------estDeterministe----------")
+print("l'automate Automate A est-il déterministe ?")
+print(Automate.estDeterministe(aAutomate))
+
+#completeAutomate
+print("\n")
+print("----------completeAutomate----------")
+print("Voici l'automate complété :")
+print(Automate.completeAutomate(aAutomateComplete,"ab"))
+
+
+#determinisation
+print("\n")
+print("----------determinisation----------")
+print("Voici l'automate determinisé :")
+print(Automate.determinisation(aAutomateDeter))
+
+
+#-----------------------Automate B-----------------------
+#-----------------------Automate B-----------------------
+#-----------------------Automate B-----------------------
+print("\n")
+print("----------Automate B----------")
+print("----------Automate B----------")
+print("----------Automate B----------")
+#Pas Complet et Non Déterministe
+#Etats
+bState0 = State(0,True,False)
+bState1 = State(1,False,False)
+bState2 = State(2,False,False)
+bState3 = State(3,False,True)
+bListe_state = [bState0,bState1,bState2,bState3]
+
+#Transitions
+bTransition0 = Transition(bState0,"a",bState0)
+bTransition1 = Transition(bState0,"b",bState0)
+bTransition2 = Transition(bState0,"a",bState1)
+bTransition3 = Transition(bState1,"a",bState2)
+bTransition4 = Transition(bState2,"a",bState3)
+bTransition5 = Transition(bState3,"a",bState3)
+bTransition6 = Transition(bState3,"b",bState3)
+
+bListe_trans = [bTransition0,bTransition1,bTransition2,bTransition3,bTransition4,bTransition5,bTransition6]
+
+bAutomate = Automate(bListe_trans)
+bAutomateComplete = Automate(bListe_trans)
+bAutomateDeter = Automate(bListe_trans)
+
+print(bAutomate)
+bAutomate.show("automateB")
+
+#succ
+print("\n")
+print("----------succ----------")
+print("Quelle est la liste des états à partir de la lettre 'a' ?")
+print(bAutomate.succ(bListe_state,"a"))
+print("Quelle est la liste des états à partir de la lettre 'b' ?")
+print(bAutomate.succ(bListe_state,"b"))
+
+#accepte
+print("\n")
+print("----------accepte----------")
+print("le mot '' (vide) est-il accepté ?")
+print(Automate.accepte(bAutomate,""))
+print("le mot 'a' est-il accepté ?")
+print(Automate.accepte(bAutomate,"a"))
+print("le mot 'b' est-il accepté ?")
+print(Automate.accepte(bAutomate,"b"))
+print("le mot 'aabaabaa' est-il accepté ?")
+print(Automate.accepte(bAutomate,"aabaabaa"))
+print("le mot 'aaa' est-il accepté ?")
+print(Automate.accepte(bAutomate,"aaa"))
+print("le mot 'aabaaabbbba' est-il accepté ?")
+print(Automate.accepte(bAutomate,"aabaaabbbba"))
+
+#estComplet
+print("\n")
+print("----------estComplet----------")
+print("l'automate Automate B est-il complet ?")
+print(Automate.estComplet(bAutomate,"ab"))
+
+#estDeterministe
+print("\n")
+print("----------estDeterministe----------")
+print("l'automate Automate B est-il déterministe ?")
+print(Automate.estDeterministe(bAutomate))
+
+#completeAutomate
+print("\n")
+print("----------completeAutomate----------")
+print("Voici l'automate complété :")
+print(Automate.completeAutomate(bAutomateComplete,"ab"))
+Automate.completeAutomate(bAutomateComplete,"ab").show("CompletAutomateB")
+
+#determinisation
+print("\n")
+print("----------determinisation----------")
+print("Voici l'automate determinisé :")
+print(Automate.determinisation(bAutomateDeter))
+Automate.determinisation(bAutomateDeter).show("DeterAutomateB")
+
+
+
+#-----------------------Automate C-----------------------
+#-----------------------Automate C-----------------------
+#-----------------------Automate C-----------------------
+print("\n")
+print("----------Automate C----------")
+print("----------Automate C----------")
+print("----------Automate C----------")
+#Complet et Non Déterministe
+#Etats
+cState0 = State(0,True,False)
+cState1 = State(1,False,True)
+cListe_state = [cState0,cState1]
+
+#Transitions
+cTransition0 = Transition(cState0,"a",cState0)
+cTransition1 = Transition(cState0,"b",cState0)
+cTransition2 = Transition(cState0,"c",cState0)
+cTransition3 = Transition(cState0,"a",cState1)
+cTransition4 = Transition(cState1,"a",cState1)
+cTransition5 = Transition(cState1,"b",cState1)
+cTransition6 = Transition(cState1,"c",cState1)
+
+cListe_trans = [cTransition0,cTransition1,cTransition2,cTransition3,cTransition4,cTransition5,cTransition6]
+
+cAutomate = Automate(cListe_trans)
+cAutomateComplete = Automate(cListe_trans)
+cAutomateDeter = Automate(cListe_trans)
+
+print(cAutomate)
+cAutomate.show("automateC")
+
+#succ
+print("\n")
+print("----------succ----------")
+print("Quelle est la liste des états à partir de la lettre 'a' ?")
+print(cAutomate.succ(cListe_state,"a"))
+print("Quelle est la liste des états à partir de la lettre 'b' ?")
+print(cAutomate.succ(cListe_state,"b"))
+print("Quelle est la liste des états à partir de la lettre 'c' ?")
+print(cAutomate.succ(cListe_state,"c"))
+
+#accepte
+print("\n")
+print("----------accepte----------")
+print("le mot '' (vide) est-il accepté ?")
+print(Automate.accepte(cAutomate,""))
+print("le mot 'a' est-il accepté ?")
+print(Automate.accepte(cAutomate,"a"))
+print("le mot 'b' est-il accepté ?")
+print(Automate.accepte(cAutomate,"b"))
+print("le mot 'c' est-il accepté ?")
+print(Automate.accepte(cAutomate,"c"))
+print("le mot 'abc' est-il accepté ?")
+print(Automate.accepte(cAutomate,"abc"))
+print("le mot 'bbcc' est-il accepté ?")
+print(Automate.accepte(cAutomate,"bbcc"))
+print("le mot 'cbbcacbbc' est-il accepté ?")
+print(Automate.accepte(cAutomate,"cbbcacbbc"))
+
+#estComplet
+print("\n")
+print("----------estComplet----------")
+print("l'automate Automate C est-il complet ?")
+print(Automate.estComplet(cAutomate,"abc"))
+
+#estDeterministe
+print("\n")
+print("----------estDeterministe----------")
+print("l'automate Automate C est-il déterministe ?")
+print(Automate.estDeterministe(cAutomate))
+
+#completeAutomate
+print("\n")
+print("----------completeAutomate----------")
+print("Voici l'automate complété :")
+print(Automate.completeAutomate(cAutomateComplete,"abc"))
+#            Automate.completeAutomate(cAutomateComplete,"ab").show("CompletAutomateC")
+
+#determinisation
+print("\n")
+print("----------determinisation----------")
+print("Voici l'automate determinisé :")
+print(Automate.determinisation(cAutomateDeter))
+Automate.determinisation(cAutomateDeter).show("DeterAutomateC")
+
+#-----------------------Automate D-----------------------
+#-----------------------Automate D-----------------------
+#-----------------------Automate D-----------------------
+print("\n")
+print("----------Automate D----------")
+print("----------Automate D----------")
+print("----------Automate D----------")
+#Pas Complet et Déterministe
+#Etats
+dState0 = State(0,True,False)
+dState1 = State(1,False,False)
+dState2 = State(2,False,False)
+dState3 = State(3,False,True)
+dListe_state = [dState0,dState1,dState2,dState3]
+
+#Transitions
+dTransition0 = Transition(dState0,"b",dState1)
+dTransition1 = Transition(dState1,"a",dState2)
+dTransition2 = Transition(dState2,"c",dState3)
+
+dListe_trans = [dTransition0,dTransition1,dTransition2]
+
+dAutomate = Automate(dListe_trans)
+dAutomateComplete = Automate(dListe_trans)
+dAutomateDeter = Automate(dListe_trans)
+
+print(dAutomate)
+dAutomate.show("automateD")
+
+#succ
+print("\n")
+print("----------succ----------")
+print("Quelle est la liste des états à partir de la lettre 'a' ?")
+print(dAutomate.succ(dListe_state,"a"))
+print("Quelle est la liste des états à partir de la lettre 'b' ?")
+print(dAutomate.succ(dListe_state,"b"))
+print("Quelle est la liste des états à partir de la lettre 'c' ?")
+print(dAutomate.succ(dListe_state,"c"))
+
+#accepte
+print("\n")
+print("----------accepte----------")
+print("le mot '' (vide) est-il accepté ?")
+print(Automate.accepte(dAutomate,""))
+print("le mot 'a' est-il accepté ?")
+print(Automate.accepte(dAutomate,"a"))
+print("le mot 'b' est-il accepté ?")
+print(Automate.accepte(dAutomate,"b"))
+print("le mot 'c' est-il accepté ?")
+print(Automate.accepte(dAutomate,"c"))
+print("le mot 'abc' est-il accepté ?")
+print(Automate.accepte(dAutomate,"abc"))
+print("le mot 'bac' est-il accepté ?")
+print(Automate.accepte(dAutomate,"bac"))
+
+#estComplet
+print("\n")
+print("----------estComplet----------")
+print("l'automate Automate D est-il complet ?")
+print(Automate.estComplet(dAutomate,"abc"))
+
+#estDeterministe
+print("\n")
+print("----------estDeterministe----------")
+print("l'automate Automate C est-il déterministe ?")
+print(Automate.estDeterministe(dAutomate))
+
+#completeAutomate
+print("\n")
+print("----------completeAutomate----------")
+print("Voici l'automate complété :")
+print(Automate.completeAutomate(dAutomateComplete,"abc"))
+Automate.completeAutomate(dAutomateComplete,"ab").show("CompletAutomateD")
+
+#determinisation
+print("\n")
+print("----------determinisation----------")
+print("Voici l'automate determinisé :")
+print(Automate.determinisation(dAutomateDeter))
+#          Automate.determinisation(dAutomateDeter).show("DeterAutomateD")
